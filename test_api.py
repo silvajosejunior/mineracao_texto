4d7b34202c08ee10949f22a99d33212e7523951a
jose junior <josejunioroliveirasilva@gmail.com>
2023-11-12 12:53:45 -0400

    Update test_api.py
    correção de código

--- test_api.py
+++ test_api.py
@@ -1,11 +1,11 @@
 import requests
 
-# Fazer uma solicitação GET para a raiz da API
+
 response = requests.get("http://localhost:5000/")
 print("Resposta da raiz da API:")
 print(response.text)
 
-# Fazer uma solicitação POST para a rota de classificação de emoções
+
 frase_para_classificar = "tristeza"
 data = {"frase": frase_para_classificar}
 response = requests.post("http://localhost:5000/classificar_emocao", json=data)
